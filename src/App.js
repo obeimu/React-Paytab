700e2e8df78d6dcbb1fa98d2f5ac945cf02c7cd8
obeimu <obei@syndo.app>
2022-01-28 19:05:08 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form
+        action='https://webhook.site/32121b6c-6cdd-4a9d-9265-1d5ede20df9a'
+        id='payform'
+        name='payform'
+        method='post'
+      >
+        <span id='paymentErrors'></span>
+        <div className='row'>
+          <label>Card Number</label>
+          <input type='text' data-paylib='number' size='20' />
+        </div>
+        <div className='row'>
+          <label>Expiry Date (MM/YYYY)</label>
+          <input type='text' data-paylib='expmonth' size='2' />
+          <input type='text' data-paylib='expyear' size='4' />
+        </div>
+        <div className='row'>
+          <label>Security Code</label>
+          <input type='text' data-paylib='cvv' size='4' />
+        </div>
+        <input type='submit' value='Place order' />
+      </form>
     </div>
   );
 }
